5aa5bd0a782fb05fc82e6e677e17c1a084a27dc7
Нурдин Абдрасулов <=>
2020-09-16 23:55:06 +0600

    sovled problem  1182 - Column in Array

---
+++ week1/ColumnInArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ColumnInArray{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+	int columnNum = scanner.nextInt();
+	char operation = scanner.next().charAt(0);
+
+	double[][] array = new double[12][12];
+	double result = 0;         
+	for(int i = 0; i < 12; i ++)
+	    for(int j = 0; j < 12; j ++){
+		array[i][j] = scanner.nextDouble();
+	        if(j == columnNum) 
+		   result += array[i][j];	
+	    }
+	                 
+
+	if(operation == 'S')
+	    System.out.println(String.format("%.1f", result)); 	    
+         else if(operation == 'M')
+	    System.out.println(String.format("%.1f", result / 12));       
+
+    }         
+}
