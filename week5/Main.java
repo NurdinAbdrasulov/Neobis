9bda8020e540db4c57fb6402b9c5c23e1db51829
Нурдин Абдрасулов <=>
2020-10-20 02:21:29 +0600

    minnor fix

--- week5/Main.java
+++ week5/Main.java
@@ -1,4 +1,10 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,21 +23,25 @@ public class Main {
                     System.out.println();
                 }
                 System.out.println();
-
-                resultSet.close();
             } catch (SQLException e){
                 e.printStackTrace();
+            } finally {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
         addNewCar("brand1", "model", "bodyStyle", "2014-01-28", "color", 123D);
         deleteCarById(10);
-        updateCarsBrand("newBrand1", 5);
+        updateCarsBrand("newBrand5", 5);
 
     }
 
     /***
-     * Подключается к базе данных 
+     * Подключается к базе данных и возвращает объект connection
      */
     private static Connection getConnection(){
         final String databaseURL = "jdbc:mysql://localhost:3306/week4";
@@ -41,7 +51,7 @@ public class Main {
         Connection connection = null;
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");// com.mysql.cj.jdbc.Driver   com.mysql.jdbc.Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");//com.mysql.jdbc.Driver
             connection = DriverManager.getConnection(databaseURL,userName,password);
         } catch (ClassNotFoundException e){
             e.printStackTrace();
@@ -58,20 +68,24 @@ public class Main {
      */
     private static void updateCarsBrand(String brand, long id){
         Connection connection = getConnection();
+
         if(connection != null) {
+            PreparedStatement preparedStatement = null;
             try {
-                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE cars SET brand = ? WHERE id = ?");
+                preparedStatement = connection.prepareStatement("UPDATE cars SET brand = ? WHERE id = ?");
                 preparedStatement.setString(1, brand);
                 preparedStatement.setDouble(2, id);
 
                 int val = preparedStatement.executeUpdate();
-                preparedStatement.close();
                 System.out.println(val + " records were updated");
-
-                connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             } finally {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
                 try {
                     connection.close();
                 } catch (SQLException e) {
@@ -86,21 +100,24 @@ public class Main {
      * @param id - id записи, которую требуется удалить
      */
     private static void deleteCarById(long id){
-        PreparedStatement preparedStatement = null;
-
         Connection connection = getConnection();
+
         if(connection != null) {
+            PreparedStatement preparedStatement = null;
             try {
                 preparedStatement = connection.prepareStatement("DELETE FROM cars WHERE id = ?;");
                 preparedStatement.setLong(1, id);
 
                 int val = preparedStatement.executeUpdate();
-                preparedStatement.close();
                 System.out.println(val + " records were deleted");
-
             } catch (SQLException e) {
                 e.printStackTrace();
             } finally {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
                 try {
                     connection.close();
                 } catch (SQLException e) {
@@ -119,9 +136,11 @@ public class Main {
      */
     private static void addNewCar(String brand, String model, String bodyStyle, String yearOfManufacture, String color, double price) {
         Connection connection = getConnection();
+
         if(connection != null) {
+            PreparedStatement preparedStatement = null;
             try {
-                PreparedStatement preparedStatement = connection.prepareStatement("insert into cars (brand, model, body_style, year_of_manufacture, color, price) values(?, ?, ?, ?, ?, ?)");
+                preparedStatement = connection.prepareStatement("insert into cars (brand, model, body_style, year_of_manufacture, color, price) values(?, ?, ?, ?, ?, ?)");
                 preparedStatement.setString(1, brand);
                 preparedStatement.setString(2, model);
                 preparedStatement.setString(3, bodyStyle);
@@ -130,11 +149,15 @@ public class Main {
                 preparedStatement.setDouble(6, price);
 
                 int val = preparedStatement.executeUpdate();
-                preparedStatement.close();
                 System.out.println(val + " records were added to cars");
             } catch (SQLException e) {
                 e.printStackTrace();
             } finally {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
                 try {
                     connection.close();
                 } catch (SQLException e) {
@@ -155,14 +178,19 @@ public class Main {
 
         Connection connection = getConnection();
         if(connection != null) {
-            String query = "select * from " + tableName;
+            String query = "SELECT * FROM " + tableName;
+            Statement statement = null;
             try {
-                Statement statement = connection.createStatement();
+                statement = connection.createStatement();
                 resultSet = statement.executeQuery(query);
-                statement.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             } finally {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
                 try {
                     connection.close();
                 } catch (SQLException e) {
