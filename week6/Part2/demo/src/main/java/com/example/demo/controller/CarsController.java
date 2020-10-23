c73c19eb191385fea25d8636841dce3b7272dda5
Нурдин Абдрасулов <=>
2020-10-23 22:32:52 +0600

    wrote text.txt

--- week6/Part2/demo/src/main/java/com/example/demo/controller/CarsController.java
+++ week6/Part2/demo/src/main/java/com/example/demo/controller/CarsController.java
@@ -16,18 +16,33 @@ public class CarsController {
     private CarsService carsService;
 
 
+    /***
+     * Simple get request that
+     * @return all existing cars
+     */
     @GetMapping("/getAllCars")
     public @ResponseBody Iterable<Cars> getAllCpu() {
         Iterable<Cars> values =  carsService.getAllCars();
         return values;
     }
 
+    /**
+     * add new record to database
+     * @param carModel - new record
+     * @return message
+     */
     @PostMapping("/addCar")
     public String  addCar(@ModelAttribute CarModel carModel){
         carsService.addNewCar(carModel);
         return "New car was successfully added";
     }
 
+    /***
+     * find record by id and set new values
+     * @param id - id of existing record, that should be modified
+     * @param carModel - new values for record
+     * @return message
+     */
     @PutMapping("/update/{id}")
     public String updateCar(@PathVariable("id") int id, @ModelAttribute CarModel carModel){
         carsService.updateById(id, carModel);
@@ -35,6 +50,11 @@ public class CarsController {
     }
 
 
+    /***
+     * find record by id and delete it
+     * @param id - if of record that should be deleted
+     * @return message
+     */
     @DeleteMapping("/delete/{id}")
     public String removeCar(@PathVariable("id") int id){
         carsService.removeById(id);
